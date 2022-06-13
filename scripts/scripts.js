55459e810366b39af81edd7b3d793bb86fe5caed
Leonardo <leonardo.fg95@gmail.com>
2022-06-12 21:29:57 -0300

    Criação do CSS para dispositvos médios e grandes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,20 +8,20 @@ $('#btn-mobile').click(function(){
 // Efeito maquina de escrever
 
 
-function typeWrite(elemento){
-    const textoArray = elemento.innerHTML.split('');
-    elemento.innerHTML = ' ';
-    textoArray.forEach(function(letra, i){   
+// function typeWrite(elemento){
+//     const textoArray = elemento.innerHTML.split('');
+//     elemento.innerHTML = ' ';
+//     textoArray.forEach(function(letra, i){   
       
-    setTimeout(function(){
-        elemento.innerHTML += letra;
-    }, 100 * i)
+//     setTimeout(function(){
+//         elemento.innerHTML += letra;
+//     }, 100 * i)
 
-  });
-}
+//   });
+// }
 
-const titulo = document.querySelector('.job');
+// const titulo = document.querySelector('.job');
 
-$(window).on("load", function(){
-    typeWrite(titulo)
- });
+// $(window).on("load", function(){
+//     typeWrite(titulo)
+//  });
