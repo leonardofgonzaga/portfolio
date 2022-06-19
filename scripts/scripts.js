d644ae68a8344982d15db7dd2f54ce39b29ef8d9
Leonardo <leonardo.fg95@gmail.com>
2022-06-19 15:54:15 -0300

    Slider

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,27 +1,37 @@
-//Mostrar bara de navegação lateral
+// Mostrar bara de navegação lateral
 
 $('#btn-mobile').click(function(){
     $('#menu').toggleClass('mostrar')
     $('#btn-mobile').toggleClass('transformar')
 })
 
-// Efeito maquina de escrever
+// Slider
 
+let slideIndex = 1;
+showSlides(slideIndex);
 
-// function typeWrite(elemento){
-//     const textoArray = elemento.innerHTML.split('');
-//     elemento.innerHTML = ' ';
-//     textoArray.forEach(function(letra, i){   
-      
-//     setTimeout(function(){
-//         elemento.innerHTML += letra;
-//     }, 100 * i)
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-//   });
-// }
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-// const titulo = document.querySelector('.job');
-
-// $(window).on("load", function(){
-//     typeWrite(titulo)
-//  });
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("my-slides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
